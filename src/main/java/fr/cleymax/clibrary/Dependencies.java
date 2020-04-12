17f88c3d1534dd0d82722dce3e9bbc4609c34902
Clément P. (Cleymax) <clemperrin1@gmail.com>
2020-04-12 14:16:55 +0200

    :rocket: Add Dependencies

---
+++ src/main/java/fr/cleymax/clibrary/Dependencies.java
@@ -0,0 +1,23 @@
+package fr.cleymax.clibrary;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * File <b>Dependencies</b> located on fr.cleymax.clibrary is a part of CLibrary. Created the 12/04/2020
+ * <p>
+ * @author Clément P. (Cleymax) Class to load multiple dependencies.
+ */
+@Documented
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface Dependencies {
+
+	/**
+	 * A table representing a list of dependencies.
+	 * @return dependencies.
+	 */
+	Dependency[] value();
+}
